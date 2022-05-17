aec5d1864085a9837bb02fc34621a3a8d9bc3ce7
Adripa <hlmkadri@gmail.com>
2022-05-17 12:06:15 +0200

    primera prueba

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = requite('express')
+const serveStatic = require('serve-static')
+const path = require('path')
+
+const app = express()
+
+app.use('/', serveStatic(path.join(__dirname, '/dist')))
+
+app.get(/.*/, function(req,res){
+    res.sendFile(path.join(__dirname, '/dist/index.html'))
+})
+
+const port = process.env.PORT || 8080
+app.list(port)
+console.log(`app is listening on port: ${port}`)
